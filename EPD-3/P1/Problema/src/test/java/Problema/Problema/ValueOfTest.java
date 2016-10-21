6f9e5e224f4d340666cf839658e7eb979e0fb5d1
ngamort <nereagameroortega@hotmail.com>
2016-10-21 12:53:42 +0200

    ValueOfTest

--- EPD-3/P1/Problema/src/test/java/Problema/Problema/ValueOfTest.java
+++ EPD-3/P1/Problema/src/test/java/Problema/Problema/ValueOfTest.java
@@ -19,5 +19,19 @@ public class ValueOfTest {
 	  Assert.assertTrue(parseable);
 	  Assert.assertTrue(16==Integer.valueOf(16));
   }
-  
+  @Test
+  public void comprobarValorFloat() {
+	  boolean parseable = true;
+	    try{
+	        Float aux=Float.valueOf("5.2");
+	        if(aux instanceof Float){     	
+	        	System.out.println("Se ha realizado correctamente");
+	        }
+	    }catch(NumberFormatException e){
+	        parseable = false;
+	    }
+	    System.out.println(Float.valueOf("5.2"));
+	  Assert.assertTrue(parseable);
+	 Assert.assertTrue((float)5.2==Float.valueOf("5.2"));
+  }
 }
